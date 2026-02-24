ba54786d54fb3c30cd293f812e2ab8b44d4ab469
Tuan Pham <pmanht@gmail.com>
2026-02-25 02:04:16 +0700

    assignment4 is done

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -19,24 +19,47 @@ Corpus tokenize(std::string& source) {
 
   Corpus tokens;
 
-  auto createToken = [&](auto start, auto end) {
+  auto create_token = [&](auto start, auto end) {
     return Token(source, start, end);
   };
 
   std::transform(spaces.begin(), spaces.end()-1, spaces.begin()+1, 
-                 std::inserter(tokens, tokens.end()), createToken);
+                 std::inserter(tokens, tokens.end()), create_token);
 
-  auto checkContent = [](Token t) {
-    return t.content.size() < 1;
+  auto check_content = [](Token t) {
+    return t.content.empty();
   };
 
-  std::erase_if(tokens, checkContent);
+  std::erase_if(tokens, check_content);
   return tokens;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
-  /* TODO: Implement this method */
-  return std::set<Misspelling>();
+  namespace rv = std::ranges::views;
+  auto misspelled_view = rv::filter(source, [&](Token t) { 
+    return !dictionary.contains(t.content);
+  });
+
+  if (misspelled_view.empty()) {
+    return std::set<Misspelling>();
+  }
+
+  auto create_misspelling = [&](Token t) {
+    auto suggestion_view = rv::filter(dictionary, [&t](std::string word) {
+      return levenshtein(t.content, word) == 1;
+    });
+
+    std::set<std::string> suggestions(suggestion_view.begin(), suggestion_view.end());
+    return Misspelling(t, suggestions);
+  };
+
+  auto transform_view = misspelled_view | rv::transform(create_misspelling);
+  auto filtered_view = rv::filter(transform_view, [](Misspelling m) {
+    return !m.suggestions.empty();
+  });
+
+  std::set<Misspelling> misspelled_words(filtered_view.begin(), filtered_view.end());
+  return misspelled_words;
 };
 
 /* Helper methods */
